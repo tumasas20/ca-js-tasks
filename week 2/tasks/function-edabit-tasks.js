399e28ed1285a1e19310467159a0bd0098717daf
tumasas20 <tumasas20@gmail.com>
2022-11-28 19:19:23 +0200

    week 2: function-edabit-tasks (iki 16.45)

---
+++ week 2/tasks/function-edabit-tasks.js
@@ -0,0 +1,109 @@
+console.group('https://edabit.com/challenge/ARr5tA458o2tC9FTN');
+{
+  function hello() {
+    return "hello edabit.com";
+  }
+
+  const result = hello();
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/3LpBLgNRyaHMvNb4j');
+{
+  function addition(a, b) {
+    let sum = a + b;
+    return sum;
+  }
+
+  const result = addition(8, 4);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/8q54MKnRrm89pSLmW');
+{
+  function convert(minutes) {
+    let sec = minutes * 60;
+    return sec;
+  }
+  const result = convert(60);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/NAQhEoxbofPidLxm9');
+{
+  function addition(num) {
+    let sum = num += 1;
+    return sum;
+  }
+  const result = addition(10);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/3CaszbdZYGN4otQD8');
+{
+  function triArea(base, height) {
+    let answer = (base * height) / 2;
+    return answer;
+  }
+  const result = triArea(12, 2);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw');
+{
+  function calcAge(age) {
+    let days = age * 365;
+    if(age >= 0){
+      return days;
+    }
+  }
+  const calc = calcAge(34);
+  console.log(calc);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/j7yQbF3J3rToHsDBP');
+{
+  function calcAge(age) {
+    let days = age * 365;
+    if(age >= 0){
+      return days;
+    }
+  }
+  const calc = calcAge(34);
+  console.log(calc);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/j7yQbF3J3rToHsDBP');
+{
+  function cubes(a) {
+    return a ** 3;
+  }
+  const result = cubes(5);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/QaApgtePE6QrCZ64o');
+{
+  function getFirstValue(arr) {
+    return arr[0];
+  }
+  const firstNum = getFirstValue([10]);
+  console.log(firstNum);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/wAdE9te55cowBLcPs');
+{
+  function circuitPower(voltage, current) {
+	
+  }
+}
+console.groupEnd();
