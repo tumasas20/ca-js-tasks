dbcbc459259ed31a6a419bd389978b57107acbc4
tumasas20 <tumasas20@gmail.com>
2022-11-29 09:35:59 +0200

    week 2: function-edabit-tasks ( + 2val )

--- week 2/tasks/function-edabit-tasks.js
+++ week 2/tasks/function-edabit-tasks.js
@@ -58,7 +58,7 @@ console.group('https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw');
 {
   function calcAge(age) {
     let days = age * 365;
-    if(age >= 0){
+    if (age >= 0) {
       return days;
     }
   }
@@ -71,7 +71,7 @@ console.group('https://edabit.com/challenge/j7yQbF3J3rToHsDBP');
 {
   function calcAge(age) {
     let days = age * 365;
-    if(age >= 0){
+    if (age >= 0) {
       return days;
     }
   }
@@ -103,7 +103,138 @@ console.groupEnd();
 console.group('https://edabit.com/challenge/wAdE9te55cowBLcPs');
 {
   function circuitPower(voltage, current) {
-	
+    let circuitPower = voltage * current;
+    return circuitPower;
   }
+  const result = circuitPower(230, 10);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/6AnQqiEjkJdZrWhPS');
+{
+  function howManySeconds(hours) {
+    let secHaveHour = hours * 60 * 60;
+    return secHaveHour;
+  }
+  const result = howManySeconds(24);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/nhXofMMyrowMyr9Nv');
+{
+  function nextEdge(side1, side2) {
+    let thirdEdge = (side1 + side2) - 1;
+    if (thirdEdge > 0) {
+      return thirdEdge;
+    }
+  }
+  const result = nextEdge(5, 2);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/Q2j5FTFtsk7PdzrQk');
+{
+  function remainder(x, y) {
+    let num = x % y;
+    return num;
+  }
+  const result = remainder(5, 5);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/XnJ24rWW7iJkNrtsh');
+{
+  function findPerimeter(length, width) {
+    let perimeter = length * 2 + width * 2;
+    return perimeter;
+  }
+  const result = findPerimeter(20, 10);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/MvZK536X7fyrWH8Qc');
+{
+  function giveMeSomething(a) {
+    let something = "something " + a;
+    return something;
+  }
+  const result = giveMeSomething('Bob Jane');
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/ks3vMrqdnW3CQ3F4i');
+{
+  function squaed(b) {
+    return b * b;
+  }
+  const result = squaed(7);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/PTiLYyb4A69KZtBCg');
+{
+  function lessThanOrEqualToZero(num) {
+    if (num <= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const result = lessThanOrEqualToZero(-1);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG');
+{
+  function sumPolygon(n) {
+    return polygon = (n - 2) * 180;
+  }
+  const result = sumPolygon(4);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/Y46Xp2pcvTB77bmdD');
+{
+  function points(twoPointers, threePointers) {
+    return sum = twoPointers * 2 + threePointers * 3;
+  }
+  const result = points(38, 8);
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/ZNwHGgHvsdnYwJ5WK');
+{
+  function nameString(name) {
+    let b = "Edabit";
+    let result = name + b;
+    return result;
+  }
+  const result = nameString('JavaScript');
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('https://edabit.com/challenge/9MjEpkL7yAjAqiH58');
+{
+  function lessThan100(a, b) {
+    let sum = a + b;
+    if (sum < 100) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const result = lessThan100(20, 90);
+  console.log(result);
 }
 console.groupEnd();
